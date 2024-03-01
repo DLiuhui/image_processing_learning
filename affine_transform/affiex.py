25bec653d219ace7dc99f1a0fef84948b4eb55ba
Liuhui Ding <LiuhuiDing1996@gmail.com>
2024-03-01 18:08:10 +0800

    add-crop & resize

--- affine_transform/affiex.py
+++ affine_transform/affiex.py
@@ -6,11 +6,11 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--image', type=str, default='./datas/timg.jpg', help='')
-    parser.add_argument('--out-path', type=str, default='./datas', help='')
-    parser.add_argument('--bbox', type=str, default='110,60,400,290', help='')
-    parser.add_argument('--rotate-angle', type=float, default=10, help='')
-    parser.add_argument('--out-wh', type=str, default='250,270', help='')
+    parser.add_argument('--image', type=str, default='./timg.jpg', help='')
+    parser.add_argument('--out-path', type=str, default='./', help='')
+    parser.add_argument('--crop-param', type=str, default='110,60,400,290', help='')
+    parser.add_argument('--rotate-angle', type=float, default=30, help='')
+    parser.add_argument('--output-size', type=str, default='300,200', help='')
     parser.add_argument('--shear-factor', type=float, default=0.1, help='')
     return parser.parse_args()
 
@@ -19,35 +19,37 @@ if __name__ == '__main__':
 
     img = cv2.imread(args.image, 1)
     img_h, img_w = img.shape[:2]
-
     # cv2.imshow('original image', img)
     # cv2.waitKey(0)
-    
+
+    # crop
     # [左上角x, 左上角y, 裁切宽w, 裁切长h]
-    bbox = [int(e) for e in args.bbox.split(',')]
-    out_wh = [int(e) for e in args.out_wh.split(',')]
-
+    crop_param = [int(e) for e in args.crop_param.split(',')]
     # crop matrix, mapping the center of bbox to the center of croped image
-    crop_mat = np.zeros((3, 3), np.float32)
-    crop_mat[0][0] = 1
-    crop_mat[1][1] = 1
-    crop_mat[2][2] = 1
+    crop_mat = np.array(
+        [[1.0, 1.0, -crop_param[0]],
+         [1.0, 1.0, -crop_param[1]]], np.float32)
 
-    crop_mat[0][2] = -bbox[0]
-    crop_mat[1][2] = -bbox[1]
-
-    crop_out = img[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]]
+    crop_out = img[crop_param[1]:crop_param[1] + crop_param[3], crop_param[0]:crop_param[0] + crop_param[2]]
     cv2.imwrite(os.path.join(args.out_path, "cropped.png"), crop_out)
 
+    out_img = cv2.warpAffine(img, crop_mat, (crop_param[3], crop_param[2]))
+    cv2.imwrite(os.path.join(args.out_path, "affine_cropped.png"), out_img)
+
+    # resize
+    out_wh = [int(e) for e in args.output_size.split(',')]
+    scale_x = float(out_wh[0]) / crop_param[2]
+    scale_y = float(out_wh[1]) / crop_param[3]
     # scale matrix
-    scale_mat = np.zeros((3, 3), np.float32)
-    scale_mat[2][2] = 1
+    scale_mat = np.array(
+        [[scale_x, 1.0, 0],
+         [1.0, scale_y, 0]], np.float32)
 
-    scale_x = float(out_wh[0]) / bbox[2]
-    scale_y = float(out_wh[1]) / bbox[3]
+    resize_out = cv2.resize(crop_out, (out_wh[0], out_wh[1]), cv2.INTER_LINEAR)
+    cv2.imwrite(os.path.join(args.out_path, "resized.png"), crop_out)
 
-    scale_mat[0][0] = scale_x
-    scale_mat[1][1] = scale_y
+    out_img = cv2.warpAffine(img, crop_mat, (crop_param[3], crop_param[2]))
+    cv2.imwrite(os.path.join(args.out_path, "affine_cropped.png"), out_img)
 
     # shift matrix 
     shift_mat1 = np.zeros((3, 3), np.float32)
